cdefa563897787695daf5da396fbff5b9043c07d
Крылов Антон Юрьевич <krylov.au@kontur>
2021-07-21 22:43:42 +0500

    19: added routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import { BrowserRouter, Redirect, Route } from 'react-router-dom'
 import './App.css'
 import Dialogs from './components/Dialogs/Dialogs'
 import Header from './components/Header/Header'
+import Music from './components/Music/Music'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
+import Settings from './components/Settings/Settings'
+import News from './components/News/News';
 
 const App = () => {
   return (
-    <div className="app_wrapper">
-      <Header />
-      <Navbar />
-      <div className="app_wrapper_content">
-        {/* <Profile /> */}
-        <Dialogs />
-      </div> 
-    </div>
+    <BrowserRouter>
+      <div className="app_wrapper">
+        <Header />
+        <Navbar />
+        <div className="app_wrapper_content">
+          <Route component={Profile} path="/profile"/>
+          <Route component={Dialogs} path="/messages"/>  
+          <Route component={Music} path="/music"/>  
+          <Route component={Settings} path="/settings"/>  
+          <Route component={News} path="/news"/>
+          <Redirect to="/profile" path="/" />  
+          
+        </div> 
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,9 @@
+import styles from './Music.module.css'
+
+const Music = (props) => {
+  return (
+    <h1>Music</h1>
+  )
+}
+
+export default Music
