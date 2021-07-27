2e60b2e2eabc12fdbc24eb013540e2e8502eeb41
FCLans <fclanss@yandex.ru>
2021-07-27 10:48:36 +0500

    31: onClick, ref in dialogs and profile pages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,30 @@
 import Dialog from './Dialog/Dialog'
 import styles from './Dialogs.module.css'
 import Message from './Message/Message'
+import React from 'react'
+
 
 const Dialogs = props => {
   let dialogsElements = props.data.dialogsData.map(d => <Dialog key={d.id} name={d.name} id={d.id} />)
   let messagesElements = props.data.messagesData.map(m => <Message key={m.id} text={m.text} />)
+  const textNewMessage = React.createRef()
+
+  const sendMessage = () => {
+    if (textNewMessage.current.value) {
+      alert(textNewMessage.current.value)
+      textNewMessage.current.value = ''
+    }
+  }
 
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogs_items}>{dialogsElements}</div>
-      <div className={styles.messages}>{messagesElements}</div>
+      <div className={styles.messages}>
+        {messagesElements}
+        <br/>
+        <div><textarea ref={textNewMessage}/></div>
+        <button onClick={sendMessage}>Send message</button>
+      </div>
     </div>
   )
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,27 @@
 import styles from './MyPosts.module.css'
 import Post from './Post/Post'
+import React from "react";
+
 
 const MyPosts = props => {
   let postsElements = props.data.postsData.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount} />)
+  let textPost = React.createRef()
+
+  const addPost = () => {
+    if (textPost.current.value) {
+      alert(textPost.current.value)
+      textPost.current.value = ''
+    }
+  }
 
   return (
     <div>
       <div>
-        <textarea />
+        <textarea ref={textPost}/>
       </div>
 
       <div>
-        <button>Add post</button>
+        <button onClick={addPost}>Add post</button>
       </div>
 
       <div className={styles.posts}>{postsElements}</div>
