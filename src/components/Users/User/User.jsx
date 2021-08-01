50ed3d297564c76a7b4736c4d5fe7296dee87c57
Krylov Anton <krylov.au@skbkontur.ru>
2021-08-01 18:16:04 +0500

    48: install axios, get users - get request.

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from '../Users.module.css'
+import userPhoto from '../../../assets/img/user.jpg'
 
 const User = (props) => {
   return (
     <div className={styles.userBlock}>
       <div className={styles.leftBlock}>
-        <div className={styles.avatar}><img src={props.user.userPhotoUrl} alt="avatar"/></div>
+        <div className={styles.avatar}><img src={props.user.photos.small !== null ? props.user.photos.small : userPhoto} alt="avatar"/></div>
         <div className={styles.following}>
           {
             props.user.followed ? <button onClick={() => {props.unfollow(props.user.id)}}>Отписаться</button> :
@@ -15,12 +16,12 @@ const User = (props) => {
       </div>
       <div className={styles.rightBlock}>
         <div className={styles.userInfo}>
-          <div className={styles.userName}>{props.user.fullName}</div>
+          <div className={styles.userName}>{props.user.name}</div>
           <div className={styles.userStatus}>{props.user.status}</div>
         </div>
         <div className={styles.locationBlock}>
-          <div>{props.user.location.city}</div>
-          <div>{props.user.location.country}</div>
+          <div>{"props.user.location.city"}</div>
+          <div>{"props.user.location.country"}</div>
         </div>
       </div>
     </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,37 +1,13 @@
 import React from 'react'
 import User from './User/User';
 import styles from './Users.module.css'
+import * as axios from 'axios'
 
 const Users = (props) => {
   if (props.users.length === 0) {
-    const users = [
-      {
-        id: 1,
-        followed: true,
-        userPhotoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Emperor_penguin.jpg/273px-Emperor_penguin.jpg',
-        fullName: 'Иван',
-        status: 'Если тебе плюют в спину, значит ты впереди.',
-        location: {city: 'Москва', country: 'Россия'}
-      },
-      {
-        id: 2,
-        fallowed: false,
-        userPhotoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Emperor_penguin.jpg/273px-Emperor_penguin.jpg',
-        fullName: 'Вовка',
-        status: 'У всякого безумия есть своя логика.',
-        location: {city: 'Екатеринбург', country: 'Россия'}
-      },
-      {
-        id: 3,
-        fallowed: false,
-        userPhotoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Emperor_penguin.jpg/273px-Emperor_penguin.jpg',
-        fullName: 'Андрей',
-        status: 'Чтобы выигрывать, прежде всего нужно играть.',
-        location: {city: 'Киев', country: 'Украина'}
-      },
-    ]
-
-    props.setUsers(users)
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      props.setUsers(response.data.items)
+    })
   }
 
   return (
