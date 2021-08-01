08a717644c38168c2eddde71c65bb07e0fbe0003
Krylov Anton <krylov.au@skbkontur.ru>
2021-08-01 20:47:42 +0500

    49: made a Users class component from a func component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,18 +3,22 @@ import User from './User/User';
 import styles from './Users.module.css'
 import * as axios from 'axios'
 
-const Users = (props) => {
-  if (props.users.length === 0) {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-      props.setUsers(response.data.items)
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+
+    axios.get('https://social-network.samuraijs.com/api/1.0/users?page=1374').then(response => {
+      this.props.setUsers(response.data.items)
     })
   }
 
-  return (
-    <div className={styles.usersBlock}>
-      {props.users.map(u => <User unfollow={props.unfollow} follow={props.follow} key={u.id} user={u}/>)}
-    </div>
-  )
+  render() {
+    return (
+      <div className={styles.usersBlock}>
+        {this.props.users.map(u => <User unfollow={this.props.unfollow} follow={this.props.follow} key={u.id} user={u}/>)}
+      </div>
+    );
+  }
 }
 
 export default Users
