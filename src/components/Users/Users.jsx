8b5c58bb163bd28c44e71041db5131ba2ce47bc6
Krylov Anton <krylov.au@skbkontur.ru>
2021-08-01 21:27:51 +0500

    50: get request move to componentDidMount in Users class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,8 @@ import styles from './Users.module.css'
 import * as axios from 'axios'
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
 
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users?page=1374').then(response => {
       this.props.setUsers(response.data.items)
     })
@@ -15,7 +14,10 @@ class Users extends React.Component {
   render() {
     return (
       <div className={styles.usersBlock}>
-        {this.props.users.map(u => <User unfollow={this.props.unfollow} follow={this.props.follow} key={u.id} user={u}/>)}
+        {this.props.users.map(u => <User unfollow={this.props.unfollow}
+                                         follow={this.props.follow}
+                                         key={u.id}
+                                         user={u}/>)}
       </div>
     );
   }
